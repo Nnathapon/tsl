bd6dbb1203d074074353d515522a765bc2358d5b
Nnathapon <6631312721@student.chula.ac.th>
2025-11-14 10:40:49 +0700

    done

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/',
-})
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// Use this structure to set the base path ONLY for the production build
+export default defineConfig(({ mode }) => {
+  const config = {
+    plugins: [react()],
+    // Default base is '/', so only change it for production
+    base: '/',
+  };
+
+  if (mode === 'production') {
+    // Set to your repository name, including the surrounding slashes
+    config.base = '/tsl/'; 
+  }
+
+  return config;
+});
